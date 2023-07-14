39729f2c1d25b3c100785dc98a37986a992f1a5c
ardayazgancs <arda_yazgan@hotmail.com>
2023-07-14 19:39:51 +0300

    Implementing input functions and making the script more dynamic to user input

--- App-2-Flatmates-Bill/main.py
+++ App-2-Flatmates-Bill/main.py
@@ -69,9 +69,21 @@ class PdfReport:
         webbrowser.open(self.filename)
 
 
-_bill = Bill(amount=120, period='April 2021')
-_flatmate1 = Flatmate(name='John', days_in_house=20)
-_flatmate2 = Flatmate(name='Marry', days_in_house=25)
+_amount = float(input('Hey user, enter the bill amount: '))
+_period = input('What is the bill period? E.g. December 2020: ')
 
-PdfReport = PdfReport(filename='PdfReport.pdf')
-PdfReport.generate(flatmate1=_flatmate1, flatmate2=_flatmate2, bill=_bill)
+_name1 = input('What is your name? ')
+_days_in_house1 = int(input(f'How many days did {_name1} stay in the house during the bill period? '))
+
+_name2 = input('What is the name of the other flatmate? ')
+_days_in_house2 = int(input(f'How many days did {_name2} stay in the house during the bill period? '))
+
+_bill = Bill(amount=_amount, period=_period)
+_flatmate1 = Flatmate(name=_name1, days_in_house=_days_in_house1)
+_flatmate2 = Flatmate(name=_name2, days_in_house=_days_in_house2)
+
+print(f'{_flatmate1.name} pays: ', _flatmate1.pays(bill=_bill, flatmate2=_flatmate2))
+print(f'{_flatmate2.name} pays: ', _flatmate2.pays(bill=_bill, flatmate2=_flatmate1))
+
+pdfReport = PdfReport(filename='PdfReport.pdf')
+pdfReport.generate(flatmate1=_flatmate1, flatmate2=_flatmate2, bill=_bill)
