2d46b98d75dd02421757b8550518ffd53a9f53d8
ardayazgancs <arda_yazgan@hotmail.com>
2023-07-15 17:25:02 +0300

    Improvement to the web page such that result branch is removed and end calculation is shown under the form

--- App-5-Flatmates-Bill-Web-App/main.py
+++ App-5-Flatmates-Bill-Web-App/main.py
@@ -16,8 +16,6 @@ class BillFormPage(MethodView):
         bill_form = BillForm()
         return render_template('bill_form_page.html', billform=bill_form)
 
-
-class ResultsPage(MethodView):
     def post(self):
         bill_form = BillForm(request.form)
 
@@ -25,11 +23,29 @@ class ResultsPage(MethodView):
         flatmate1 = flat.Flatmate(bill_form.name1.data, int(bill_form.days_in_house1.data))
         flatmate2 = flat.Flatmate(bill_form.name2.data, int(bill_form.days_in_house2.data))
 
-        return render_template('results.html',
+        return render_template('bill_form_page.html',
                                name1=flatmate1.name,
                                name2=flatmate2.name,
                                amount1=flatmate1.pays(bill, flatmate2),
-                               amount2=flatmate2.pays(bill, flatmate1))
+                               amount2=flatmate2.pays(bill, flatmate1),
+                               billform=bill_form,
+                               result=True)
+
+
+class ResultsPage(MethodView):
+    pass
+    # def post(self):
+    #     bill_form = BillForm(request.form)
+    #
+    #     bill = flat.Bill(float(bill_form.amount.data), bill_form.period.data)
+    #     flatmate1 = flat.Flatmate(bill_form.name1.data, int(bill_form.days_in_house1.data))
+    #     flatmate2 = flat.Flatmate(bill_form.name2.data, int(bill_form.days_in_house2.data))
+    #
+    #     return render_template('results.html',
+    #                            name1=flatmate1.name,
+    #                            name2=flatmate2.name,
+    #                            amount1=flatmate1.pays(bill, flatmate2),
+    #                            amount2=flatmate2.pays(bill, flatmate1))
 
 
 class BillForm(Form):
@@ -47,6 +63,6 @@ class BillForm(Form):
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
 app.add_url_rule('/bill_form', view_func=BillFormPage.as_view('bill_form_page'))
-app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
+# app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
 
 app.run(debug=True)
