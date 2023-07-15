6bd3c6065f74a0847a699ca52117dcaa636a31ca
ardayazgancs <arda_yazgan@hotmail.com>
2023-07-15 16:56:24 +0300

    Implemented feature to process user form data

--- App-5-Flatmates-Bill-Web-App/main.py
+++ App-5-Flatmates-Bill-Web-App/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask.views import MethodView
 from wtforms import Form, StringField, SubmitField
+from flatmates_bill import flat
 
 app = Flask(__name__)
 
@@ -17,7 +18,14 @@ class BillFormPage(MethodView):
 
 
 class ResultsPage(MethodView):
-    pass
+    def post(self):
+        bill_form = BillForm(request.form)
+
+        bill = flat.Bill(float(bill_form.amount.data), bill_form.period.data)
+        flatmate1 = flat.Flatmate(bill_form.name1.data, int(bill_form.days_in_house1.data))
+        flatmate2 = flat.Flatmate(bill_form.name2.data, int(bill_form.days_in_house2.data))
+
+        return f"{flatmate1.name} pays {flatmate1.pays(bill, flatmate2)}"
 
 
 class BillForm(Form):
@@ -35,5 +43,6 @@ class BillForm(Form):
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
 app.add_url_rule('/bill_form', view_func=BillFormPage.as_view('bill_form_page'))
+app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
 
 app.run(debug=True)
