961c73180db6da978d6c54415b3d064da8631d24
ardayazgancs <arda_yazgan@hotmail.com>
2023-07-15 12:38:44 +0300

    Finalization of the project with implementation for adaptation to dynamic user input

--- App-3-Project-Math-Painting/main.py
+++ App-3-Project-Math-Painting/main.py
@@ -1,9 +1,37 @@
 from canvas import Canvas
 from shapes import Rectangle, Square
+import os
 
-_canvas = Canvas(height=20, width=30, color=(255, 255, 255))
-r1 = Rectangle(x=1, y=6, height=7, width=10, color=(100, 200, 125))
-r1.draw(canvas=_canvas)
-s1 = Square(x=1, y=3, side=3, color=(0, 100, 222))
-s1.draw_canvas(canvas=_canvas)
-_canvas.make('canvas.png')
+# Get canvas width and height from the user
+canvas_width = int(input('Enter canvas width: '))
+canvas_height = int(input('Enter canvas height: '))
+
+# Make a dictionary of color codes and prompt for color
+colors = {'white': (255, 255, 255), 'black': (0, 0, 0)}
+canvas_color = input('Enter canvas color (white or black): ')
+
+# Create a canvas with user input
+canvas = Canvas(height=canvas_height, width=canvas_width, color=colors[canvas_color])
+
+while True:
+    shape_type = input('What do you like to draw? Enter quit to quit. ')
+    # Create rectangle if user typed rectangle:
+    if shape_type.lower() == 'quit':
+        break
+    else:
+        x = int(input(f'Enter x of {shape_type.lower()}: '))
+        y = int(input(f'Enter y of {shape_type.lower()}: '))
+        red = int(input(f'How much red should the {shape_type.lower()} have? '))
+        green = int(input(f'How much green? '))
+        blue = int(input(f'How much blue? '))
+        if shape_type.lower() == 'rectangle':
+            width = int(input(f'Enter width of {shape_type.lower()}: '))
+            height = int(input(f'Enter height of {shape_type.lower()}: '))
+            r1 = Rectangle(x=x, y=y, width=width, height=height, color=(red, green, blue))
+            r1.draw(canvas)
+        else:
+            side = int(input(f'Enter side length of {shape_type.lower()}: '))
+            s1 = Square(x=x, y=y, side=side, color=(red, green, blue))
+
+os.chdir('./files')
+canvas.make('canvas.png')
