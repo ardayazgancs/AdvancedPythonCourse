efb6d463815bd97eb5904c9d456c880d63ce92ea
ardayazgancs <arda_yazgan@hotmail.com>
2023-07-16 16:03:09 +0300

    Implemented dynamic timestamps for Today and Yesterday

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -2,12 +2,16 @@ from news import NewsFeed
 
 import yagmail
 import pandas
+import datetime
 
 df = pandas.read_excel('people.xlsx', nrows=3)
 
+today = datetime.datetime.now().strftime('%Y-%m-%d')
+yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+
 email = yagmail.SMTP(user='...', password='...')
 for index, row in df.iterrows():
-    news_feed = NewsFeed(interest=row['interest'], from_date='2020-11-14', to_date='2020-11-15')
+    news_feed = NewsFeed(interest=row['interest'], from_date=yesterday, to_date=today)
     email.send(to=row['email'],
                subject=f"Your {row['interest']} news for today!",
                contents=f"Hi {row['name']},\n See what's on about {row['interest']} today.\n {news_feed.get()}\nArda")
