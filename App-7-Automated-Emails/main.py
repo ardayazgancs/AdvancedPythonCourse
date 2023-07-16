1acd98fb58e06c10880f2e5877e6b424f556159b
ardayazgancs <arda_yazgan@hotmail.com>
2023-07-16 15:55:38 +0300

    Implemented feature to send emails to addresses stored in an Excel file

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -1,36 +1,13 @@
-import requests
+from news import NewsFeed
 
+import yagmail
+import pandas
 
-class NewsFeed:
-    """Representing multiple news titles and links as a single string
-    """
-    base_url = "https://newsapi.org/v2/everything?"
-    api_key = '933fd2976ad04fea9f0d53d20a72060e'
+df = pandas.read_excel('people.xlsx', nrows=3)
 
-    def __init__(self, interest, from_date, to_date, language):
-        self.interest = interest
-        self.from_date = from_date
-        self.to_date = to_date
-        self.language = language
-
-    def get(self):
-        url = f'{self.base_url}' \
-              f'qInTitle={self.interest}&' \
-              f'from={self.from_date}&' \
-              f'to={self.to_date}&' \
-              f'language={self.language}&' \
-              f'apiKey={self.api_key}'
-
-        response = requests.get(url)
-        content = response.json()
-        articles = content['articles']
-
-        email_body = ''
-        for article in articles:
-            email_body += article['title'] + '\n' + article['url'] + '\n\n'
-
-        return email_body
-
-
-news_feed = NewsFeed(interest='nasa', from_date='2023-07-15', to_date='2023-07-16', language='en')
-print(news_feed.get())
+email = yagmail.SMTP(user='...', password='...')
+for index, row in df.iterrows():
+    news_feed = NewsFeed(interest=row['interest'], from_date='2020-11-14', to_date='2020-11-15')
+    email.send(to=row['email'],
+               subject=f"Your {row['interest']} news for today!",
+               contents=f"Hi {row['name']},\n See what's on about {row['interest']} today.\n {news_feed.get()}\nArda")
