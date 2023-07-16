9ff27783c430aa933b1fd73746be0e37e9ff88d7
ardayazgancs <arda_yazgan@hotmail.com>
2023-07-16 22:09:21 +0300

    Create navigation menu for homepage

--- App-8-Instant-Dictionary-Webapp/webapp/home.py
+++ App-8-Instant-Dictionary-Webapp/webapp/home.py
@@ -4,9 +4,31 @@ import justpy as jp
 class Home:
     path = '/'
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+
+        layout = jp.QLayout(a=wp, view='hHh lpR fFf')
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_model='left', bordered=True)
+        scroller = jp.QScrollArea(a=drawer, classes='fit')
+        qlist = jp.QList(a=scroller)
+        a_classes = 'p-2 m-2 text-lg text-blue-400 hover:text-blue-700'
+        jp.A(a=qlist, text='Home', href='/', classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text='About', href='/about', classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text='Dictionary', href='/dictionary', classes=a_classes)
+
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon='menu',
+                click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text='Instant Dictionary')
+
+        container = jp.QPageContainer(a=layout)
+
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen p-2")
         jp.Div(a=div, text='This is the home page!', classes='text-4xl m-2')
         jp.Div(a=div, text="""Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
         has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
@@ -16,3 +38,7 @@ class Home:
         Aldus PageMaker including versions of Lorem Ipsum.
         """, classes='text-lg')
         return wp
+
+    @staticmethod
+    def move_drawer(widget, msg):
+        widget.drawer.value = not widget.drawer.value
