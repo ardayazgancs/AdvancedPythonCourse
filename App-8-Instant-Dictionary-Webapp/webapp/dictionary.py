2685544f0ac9a89342601603698441d5e920e392
ardayazgancs <arda_yazgan@hotmail.com>
2023-07-16 21:32:15 +0300

    Implemented output definition that will show up as user types a word

--- App-8-Instant-Dictionary-Webapp/webapp/dictionary.py
+++ App-8-Instant-Dictionary-Webapp/webapp/dictionary.py
@@ -1,3 +1,4 @@
+import definition
 import justpy as jp
 
 
@@ -11,9 +12,17 @@ class Dictionary:
         jp.Div(a=div, text='Instant English Dictionary', classes='text-4xl m-2')
         jp.Div(a=div, text='Get the definition of any English word instantly as you type.', classes='text-lg')
 
-        jp.Input(a=div, placeholder='Type in a word here...',
-                 classes='m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 focus:bg-white focus:outline-none '
-                         'focus:border-purple-500 py-2 px-4')
-        jp.Div(a=div, classes='m-2 p-2 text-lg border-2 h-40')
+        input_div = jp.Div(a=div, classes='grid grid-cols-2')
+
+        output_div = jp.Div(a=div, classes='m-2 p-2 text-lg border border-blue-500 h-40')
+        input_box = jp.Input(a=input_div, placeholder='Type in a word here...', outputdiv=output_div,
+                             classes='m-2 bg-gray-100 border border-gray-500 rounded w-64 focus:bg-white '
+                                     'focus:outline-none focus:border-purple-500 py-2 px-4')
+        input_box.on('input', cls.get_definition)
 
         return wp
+
+    @staticmethod
+    def get_definition(widget, msg):
+        defined = definition.Definition(widget.value).get()
+        widget.outputdiv.text = ' '.join(defined)
