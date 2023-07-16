40e177103dd35c82cf1301bdf41b5aecd2fcfa0b
ardayazgancs <arda_yazgan@hotmail.com>
2023-07-16 20:51:21 +0300

    Implemented dictionary page

--- App-8-Instant-Dictionary-Webapp/main.py
+++ App-8-Instant-Dictionary-Webapp/main.py
@@ -1,9 +1,11 @@
 from webapp.home import Home
 from webapp.about import About
+from webapp.dictionary import Dictionary
 
 import justpy as jp
 
 jp.Route(Home.path, Home.serve)
 jp.Route(About.path, About.serve)
+jp.Route(Dictionary.path, Dictionary.serve)
 
 jp.justpy()
