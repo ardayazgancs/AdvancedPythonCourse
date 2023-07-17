e12f1d1034c14353e7035fbc0a24d5e2182d12ec
ardayazgancs <arda_yazgan@hotmail.com>
2023-07-17 10:46:00 +0300

    Finalizing the API

--- App-9-Instant-Dictionary-API/api.py
+++ App-9-Instant-Dictionary-API/api.py
@@ -2,9 +2,11 @@ import justpy as jp
 import definition
 import json
 
+
 class Api:
     """Handles requests at /api?w=word
     """
+
     @classmethod
     def serve(cls, req):
         wp = jp.WebPage()
@@ -13,11 +15,13 @@ class Api:
         defined = definition.Definition(word).get()
 
         response = {
-            "word":word,
-            "definition":defined
+            "word": word,
+            "definition": defined
         }
 
         wp.html = json.dumps(response)
         return wp
 
 
+jp.Route('/api', Api.serve)
+jp.justpy()
